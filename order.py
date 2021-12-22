91c270f985d5a6821a1a9e3d10c109ea2ad27848
George Zharinov <zharinob@mail.ru>
2021-12-22 23:12:00 +0300

    first commit

---
+++ order.py
@@ -0,0 +1,33 @@
+cook_book = {
+  'Омлет': [
+    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+    ],
+  'Утка по-пекински': [
+    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+    ],
+  'Запеченный картофель': [
+    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+    ]
+  }
+
+def get_shop_list_by_dishes(dishes, person_count):
+    order = {}
+    for dish, sostav in cook_book.items():
+        if dish in dishes:
+            for indigrients in sostav:
+                i_n = list(indigrients.values())[0]
+                q = (int(list(indigrients.values())[1]) * person_count)
+                m = list(indigrients.values())[2]
+                order[i_n] = {'measure':q, 'quantity' :m}
+    print(order)
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+
+
